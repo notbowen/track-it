d1b94342fd727267d88ca5d5784d37941d6c179c
Hu Bowen <contact@hubowen.dev>
2023-12-17 16:27:49 +0800

    fix: update login page view

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,10 +5,10 @@ import { Separator } from "@/components/ui/separator";
 export default function Auth() {
     return (
         <main className="p-12">
-            <div className="flex flex-col w-80 mx-auto">
-                <AuthButtons />
-                <Separator className="my-4" />
+            <div className="flex flex-col w-80 mx-auto xl:w-96">
                 <AuthForm />
+                <Separator className="my-4" />
+                <AuthButtons />
             </div>
         </main>
     )
