25ffc655f1d42c7e341e3a3c594e80fde7773d20
Hu Bowen <contact@hubowen.dev>
2023-12-17 23:13:08 +0800

    fix: update login page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,14 +1,22 @@
 import AuthButtons from "@/app/auth/AuthButtons";
 import { AuthForm } from "@/app/auth/AuthForm";
-import { Separator } from "@/components/ui/separator";
 
 export default function Auth() {
     return (
         <main className="p-12">
             <div className="flex flex-col w-80 mx-auto xl:w-96">
-                <AuthForm />
-                <Separator className="my-4" />
-                <AuthButtons />
+                <AuthForm/>
+                <div className="relative my-4">
+                    <div className="absolute inset-0 flex items-center">
+                        <span className="w-full border-t"/>
+                    </div>
+                    <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-background px-2 text-muted-foreground">
+                        Or continue with
+                    </span>
+                    </div>
+                </div>
+                <AuthButtons/>
             </div>
         </main>
     )
