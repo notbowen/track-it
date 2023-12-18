873c99f124b5e191396ef7ed547034080391144d
Hu Bowen <contact@hubowen.dev>
2023-12-18 12:47:41 +0800

    fix: added oauth error handling

--- app/auth/AuthButtons.tsx
+++ app/auth/AuthButtons.tsx
@@ -4,15 +4,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { Button } from "@/components/ui/button";
 import { OauthLogin } from "@/app/auth/login/OauthLogin";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
 export default function AuthButtons() {
+    const { toast } = useToast();
+
+    async function StartOauth(provider: "google" | "github") {
+        const error = await OauthLogin(provider);
+        if (!error) return;
+
+        toast({
+            variant: "destructive",
+            title: "Error",
+            description: error.message,
+            action: <ToastAction altText="Try Again">Try Again</ToastAction>
+        })
+    }
+
     return (
         <div className="flex flex-col gap-2 mb-4">
-            <Button onClick={async () => await OauthLogin("google")}>
+            <Button onClick={async () => await StartOauth("google")}>
                 <FontAwesomeIcon icon={faGoogle} className="mr-2 h-4 w-4"/>
                 Login with Google
             </Button>
-            <Button onClick={async () => await OauthLogin("github")}>
+            <Button onClick={async () => await StartOauth("github")}>
                 <FontAwesomeIcon icon={faGithub} className="mr-2 h-4 w-4"/>
                 Login with Github
             </Button>
--- app/auth/login/OauthLogin.tsx
+++ app/auth/login/OauthLogin.tsx
@@ -6,12 +6,12 @@ export async function OauthLogin(provider: "google" | "github") {
     const origin = window.location.origin;
     const supabase = createClient();
 
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    const { error } = await supabase.auth.signInWithOAuth({
         provider: provider,
         options: {
             redirectTo: `${origin}/auth/callback`
         }
     })
 
-    return data
+    return error;
 }
