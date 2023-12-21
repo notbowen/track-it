da199ea04a398d199cf1a8ce86d04f4ee5866ec1
Hu Bowen <contact@hubowen.dev>
2023-12-21 22:44:08 +0800

    fix: change button text depending on logged in status

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+
+    const { data: { user } } = await supabase.auth.getUser();
+
     return (
         <main className="py-12 sm:p-12">
             <section className="py-6 mt-12 flex flex-col items-center text-center gap-8">
@@ -13,7 +20,8 @@ export default function Home() {
                     <Link href="https://github.com/notbowen/track-it" target="_blank">Source Code</Link>
                 </Button>
                 <Button>
-                    <Link href={"/auth"} shallow>Get Started</Link>
+                    {(!user) && <Link href={"/auth"} shallow>Get Started</Link>}
+                    {(user) && <Link href={"/dashboard"} shallow>Dashboard</Link>}
                 </Button>
             </div>
         </main>
