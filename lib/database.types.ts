49db8ad8c93aa20467e92bb003ca303e5e4d8557
Hu Bowen <contact@hubowen.dev>
2024-02-19 14:45:49 +0800

    chore: reformat code

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,319 +1,315 @@
 export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json | undefined }
+    | Json[]
 
 export type Database = {
-  public: {
-    Tables: {
-      admins_groups: {
-        Row: {
-          created_at: string
-          group_id: string
-          id: string
-          user_id: string
+    public: {
+        Tables: {
+            admins_groups: {
+                Row: {
+                    created_at: string
+                    group_id: string
+                    id: string
+                    user_id: string
+                }
+                Insert: {
+                    created_at?: string
+                    group_id: string
+                    id?: string
+                    user_id: string
+                }
+                Update: {
+                    created_at?: string
+                    group_id?: string
+                    id?: string
+                    user_id?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_admins_groups_group_id_fkey"
+                        columns: ["group_id"]
+                        isOneToOne: false
+                        referencedRelation: "groups"
+                        referencedColumns: ["id"]
+                    },
+                    {
+                        foreignKeyName: "public_admins_groups_user_id_fkey"
+                        columns: ["user_id"]
+                        isOneToOne: false
+                        referencedRelation: "users"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
+            groups: {
+                Row: {
+                    allow_all: boolean
+                    created_at: string
+                    created_by: string
+                    id: string
+                    name: string
+                    short_form: string
+                }
+                Insert: {
+                    allow_all: boolean
+                    created_at?: string
+                    created_by: string
+                    id?: string
+                    name: string
+                    short_form: string
+                }
+                Update: {
+                    allow_all?: boolean
+                    created_at?: string
+                    created_by?: string
+                    id?: string
+                    name?: string
+                    short_form?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_groups_created_by_fkey"
+                        columns: ["created_by"]
+                        isOneToOne: false
+                        referencedRelation: "users"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
+            status: {
+                Row: {
+                    created_at: string
+                    id: string
+                    status: Database["public"]["Enums"]["progress"]
+                    task_id: string
+                    user_id: string
+                }
+                Insert: {
+                    created_at?: string
+                    id?: string
+                    status: Database["public"]["Enums"]["progress"]
+                    task_id: string
+                    user_id: string
+                }
+                Update: {
+                    created_at?: string
+                    id?: string
+                    status?: Database["public"]["Enums"]["progress"]
+                    task_id?: string
+                    user_id?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_status_task_id_fkey"
+                        columns: ["task_id"]
+                        isOneToOne: false
+                        referencedRelation: "tasks"
+                        referencedColumns: ["id"]
+                    },
+                    {
+                        foreignKeyName: "public_status_user_id_fkey"
+                        columns: ["user_id"]
+                        isOneToOne: false
+                        referencedRelation: "users"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
+            tasks: {
+                Row: {
+                    created_at: string
+                    due_date: string
+                    group_id: string
+                    id: string
+                    name: string
+                }
+                Insert: {
+                    created_at?: string
+                    due_date: string
+                    group_id: string
+                    id?: string
+                    name: string
+                }
+                Update: {
+                    created_at?: string
+                    due_date?: string
+                    group_id?: string
+                    id?: string
+                    name?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_tasks_group_id_fkey"
+                        columns: ["group_id"]
+                        isOneToOne: false
+                        referencedRelation: "groups"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
+            users: {
+                Row: {
+                    email: string
+                    first_name: string
+                    id: string
+                    last_name: string
+                }
+                Insert: {
+                    email: string
+                    first_name: string
+                    id: string
+                    last_name: string
+                }
+                Update: {
+                    email?: string
+                    first_name?: string
+                    id?: string
+                    last_name?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_users_id_fkey"
+                        columns: ["id"]
+                        isOneToOne: true
+                        referencedRelation: "users"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
+            users_groups: {
+                Row: {
+                    created_at: string
+                    group_id: string
+                    id: string
+                    user_id: string
+                }
+                Insert: {
+                    created_at?: string
+                    group_id: string
+                    id?: string
+                    user_id: string
+                }
+                Update: {
+                    created_at?: string
+                    group_id?: string
+                    id?: string
+                    user_id?: string
+                }
+                Relationships: [
+                    {
+                        foreignKeyName: "public_users_groups_group_id_fkey"
+                        columns: ["group_id"]
+                        isOneToOne: false
+                        referencedRelation: "groups"
+                        referencedColumns: ["id"]
+                    },
+                    {
+                        foreignKeyName: "public_users_groups_user_id_fkey"
+                        columns: ["user_id"]
+                        isOneToOne: false
+                        referencedRelation: "users"
+                        referencedColumns: ["id"]
+                    }
+                ]
+            }
         }
-        Insert: {
-          created_at?: string
-          group_id: string
-          id?: string
-          user_id: string
+        Views: {
+            [_ in never]: never
         }
-        Update: {
-          created_at?: string
-          group_id?: string
-          id?: string
-          user_id?: string
+        Functions: {
+            is_user_in_group: {
+                Args: {
+                    _user_id: string
+                    _group_id: string
+                }
+                Returns: boolean
+            }
         }
-        Relationships: [
-          {
-            foreignKeyName: "public_admins_groups_group_id_fkey"
-            columns: ["group_id"]
-            isOneToOne: false
-            referencedRelation: "groups"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "public_admins_groups_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      groups: {
-        Row: {
-          allow_all: boolean
-          created_at: string
-          created_by: string
-          id: string
-          name: string
-          short_form: string
+        Enums: {
+            progress: "Not Started" | "In Progress" | "Completed"
         }
-        Insert: {
-          allow_all: boolean
-          created_at?: string
-          created_by: string
-          id?: string
-          name: string
-          short_form: string
+        CompositeTypes: {
+            [_ in never]: never
         }
-        Update: {
-          allow_all?: boolean
-          created_at?: string
-          created_by?: string
-          id?: string
-          name?: string
-          short_form?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_groups_created_by_fkey"
-            columns: ["created_by"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      status: {
-        Row: {
-          created_at: string
-          id: string
-          status: Database["public"]["Enums"]["progress"]
-          task_id: string
-          user_id: string
-        }
-        Insert: {
-          created_at?: string
-          id?: string
-          status: Database["public"]["Enums"]["progress"]
-          task_id: string
-          user_id: string
-        }
-        Update: {
-          created_at?: string
-          id?: string
-          status?: Database["public"]["Enums"]["progress"]
-          task_id?: string
-          user_id?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_status_task_id_fkey"
-            columns: ["task_id"]
-            isOneToOne: false
-            referencedRelation: "tasks"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "public_status_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      tasks: {
-        Row: {
-          created_at: string
-          due_date: string
-          group_id: string
-          id: string
-          name: string
-        }
-        Insert: {
-          created_at?: string
-          due_date: string
-          group_id: string
-          id?: string
-          name: string
-        }
-        Update: {
-          created_at?: string
-          due_date?: string
-          group_id?: string
-          id?: string
-          name?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_tasks_group_id_fkey"
-            columns: ["group_id"]
-            isOneToOne: false
-            referencedRelation: "groups"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      users: {
-        Row: {
-          email: string
-          first_name: string
-          id: string
-          last_name: string
-        }
-        Insert: {
-          email: string
-          first_name: string
-          id: string
-          last_name: string
-        }
-        Update: {
-          email?: string
-          first_name?: string
-          id?: string
-          last_name?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_users_id_fkey"
-            columns: ["id"]
-            isOneToOne: true
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      users_groups: {
-        Row: {
-          created_at: string
-          group_id: string
-          id: string
-          user_id: string
-        }
-        Insert: {
-          created_at?: string
-          group_id: string
-          id?: string
-          user_id: string
-        }
-        Update: {
-          created_at?: string
-          group_id?: string
-          id?: string
-          user_id?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_users_groups_group_id_fkey"
-            columns: ["group_id"]
-            isOneToOne: false
-            referencedRelation: "groups"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "public_users_groups_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
     }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      is_user_in_group: {
-        Args: {
-          _user_id: string
-          _group_id: string
-        }
-        Returns: boolean
-      }
-    }
-    Enums: {
-      progress: "Not Started" | "In Progress" | "Completed"
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
 }
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    PublicTableNameOrOptions extends | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+        ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+            Database[PublicTableNameOrOptions["schema"]]["Views"])
+        : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : never
+    ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+            Row: infer R
+        }
+        ? R
+        : never
+    : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+            Database["public"]["Views"])
+        ? (Database["public"]["Tables"] &
+            Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+                Row: infer R
+            }
+            ? R
+            : never
+        : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    PublicTableNameOrOptions extends | keyof Database["public"]["Tables"]
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+        : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : never
+    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+            Insert: infer I
+        }
+        ? I
+        : never
+    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+        ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+                Insert: infer I
+            }
+            ? I
+            : never
+        : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    PublicTableNameOrOptions extends | keyof Database["public"]["Tables"]
+        | { schema: keyof Database },
+    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+        : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : never
+    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+            Update: infer U
+        }
+        ? U
+        : never
+    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+        ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+                Update: infer U
+            }
+            ? U
+            : never
+        : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
-    | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    PublicEnumNameOrOptions extends | keyof Database["public"]["Enums"]
+        | { schema: keyof Database },
+    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+        ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+        : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+    ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+        ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+        : never
