3a068282ccd86d0c79d0d70f5a92badeed3dc980
Hu Bowen <contact@hubowen.dev>
2024-02-19 14:15:06 +0800

    chore: update database types

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,10 +1,10 @@
 export type Json =
-    | string
-    | number
-    | boolean
-    | null
-    | { [key: string]: Json | undefined }
-    | Json[]
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
   public: {
@@ -47,48 +47,109 @@ export type Database = {
       }
       groups: {
         Row: {
+          allow_all: boolean
           created_at: string
+          created_by: string
           id: string
           name: string
-          only_admin_modify: boolean
           short_form: string
+        }
+        Insert: {
+          allow_all: boolean
+          created_at?: string
+          created_by: string
+          id?: string
+          name: string
+          short_form: string
+        }
+        Update: {
+          allow_all?: boolean
+          created_at?: string
+          created_by?: string
+          id?: string
+          name?: string
+          short_form?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_groups_created_by_fkey"
+            columns: ["created_by"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      status: {
+        Row: {
+          created_at: string
+          id: string
+          status: Database["public"]["Enums"]["progress"]
+          task_id: string
+          user_id: string
         }
         Insert: {
           created_at?: string
           id?: string
-          name: string
-          only_admin_modify: boolean
-          short_form: string
+          status: Database["public"]["Enums"]["progress"]
+          task_id: string
+          user_id: string
         }
         Update: {
           created_at?: string
           id?: string
-          name?: string
-          only_admin_modify?: boolean
-          short_form?: string
+          status?: Database["public"]["Enums"]["progress"]
+          task_id?: string
+          user_id?: string
         }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "public_status_task_id_fkey"
+            columns: ["task_id"]
+            isOneToOne: false
+            referencedRelation: "tasks"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "public_status_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       tasks: {
         Row: {
           created_at: string
           due_date: string
+          group_id: string
           id: string
           name: string
         }
         Insert: {
           created_at?: string
           due_date: string
+          group_id: string
           id?: string
           name: string
         }
         Update: {
           created_at?: string
           due_date?: string
+          group_id?: string
           id?: string
           name?: string
         }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "public_tasks_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       users: {
         Row: {
@@ -155,51 +216,18 @@ export type Database = {
           }
         ]
       }
-      users_tasks: {
-        Row: {
-          created_at: string
-          id: number
-          status: Database["public"]["Enums"]["progress"]
-          task_id: string
-          user_id: string
-        }
-        Insert: {
-          created_at?: string
-          id?: number
-          status?: Database["public"]["Enums"]["progress"]
-          task_id: string
-          user_id: string
-        }
-        Update: {
-          created_at?: string
-          id?: number
-          status?: Database["public"]["Enums"]["progress"]
-          task_id?: string
-          user_id?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "public_users_tasks_task_id_fkey"
-            columns: ["task_id"]
-            isOneToOne: false
-            referencedRelation: "tasks"
-            referencedColumns: ["id"]
-          },
-          {
-            foreignKeyName: "public_users_tasks_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
     }
     Views: {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      is_user_in_group: {
+        Args: {
+          _user_id: string
+          _group_id: string
+        }
+        Returns: boolean
+      }
     }
     Enums: {
       progress: "Not Started" | "In Progress" | "Completed"
@@ -211,77 +239,81 @@ export type Database = {
 }
 
 export type Tables<
-    PublicTableNameOrOptions extends | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-        | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-        ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-            Database[PublicTableNameOrOptions["schema"]]["Views"])
-        : never = never
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-          Row: infer R
-        }
-        ? R
-        : never
-    : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-            Database["public"]["Views"])
-        ? (Database["public"]["Tables"] &
-            Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-              Row: infer R
-            }
-            ? R
-            : never
-        : never
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
 
 export type TablesInsert<
-    PublicTableNameOrOptions extends | keyof Database["public"]["Tables"]
-        | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-        ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-        : never = never
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-          Insert: infer I
-        }
-        ? I
-        : never
-    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-        ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-              Insert: infer I
-            }
-            ? I
-            : never
-        : never
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
 
 export type TablesUpdate<
-    PublicTableNameOrOptions extends | keyof Database["public"]["Tables"]
-        | { schema: keyof Database },
-    TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-        ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-        : never = never
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-          Update: infer U
-        }
-        ? U
-        : never
-    : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-        ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-              Update: infer U
-            }
-            ? U
-            : never
-        : never
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
 
 export type Enums<
-    PublicEnumNameOrOptions extends | keyof Database["public"]["Enums"]
-        | { schema: keyof Database },
-    EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-        ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-        : never = never
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-    : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-        ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-        : never
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
