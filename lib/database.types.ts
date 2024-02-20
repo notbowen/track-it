14d5193290f5c9a1425eb391c492d3205d696d87
Hu Bowen <contact@hubowen.dev>
2024-02-20 10:17:52 +0800

    chore: update database types

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -7,235 +7,261 @@ export type Json =
     | Json[]
 
 export type Database = {
-    public: {
-        Tables: {
-            admins_groups: {
-                Row: {
-                    created_at: string
-                    group_id: string
-                    id: string
-                    user_id: string
-                }
-                Insert: {
-                    created_at?: string
-                    group_id: string
-                    id?: string
-                    user_id: string
-                }
-                Update: {
-                    created_at?: string
-                    group_id?: string
-                    id?: string
-                    user_id?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_admins_groups_group_id_fkey"
-                        columns: ["group_id"]
-                        isOneToOne: false
-                        referencedRelation: "groups"
-                        referencedColumns: ["id"]
-                    },
-                    {
-                        foreignKeyName: "public_admins_groups_user_id_fkey"
-                        columns: ["user_id"]
-                        isOneToOne: false
-                        referencedRelation: "users"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
-            groups: {
-                Row: {
-                    allow_all: boolean
-                    created_at: string
-                    created_by: string
-                    id: string
-                    name: string
-                    short_form: string
-                }
-                Insert: {
-                    allow_all: boolean
-                    created_at?: string
-                    created_by: string
-                    id?: string
-                    name: string
-                    short_form: string
-                }
-                Update: {
-                    allow_all?: boolean
-                    created_at?: string
-                    created_by?: string
-                    id?: string
-                    name?: string
-                    short_form?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_groups_created_by_fkey"
-                        columns: ["created_by"]
-                        isOneToOne: false
-                        referencedRelation: "users"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
-            status: {
-                Row: {
-                    created_at: string
-                    id: string
-                    status: Database["public"]["Enums"]["progress"]
-                    task_id: string
-                    user_id: string
-                }
-                Insert: {
-                    created_at?: string
-                    id?: string
-                    status: Database["public"]["Enums"]["progress"]
-                    task_id: string
-                    user_id: string
-                }
-                Update: {
-                    created_at?: string
-                    id?: string
-                    status?: Database["public"]["Enums"]["progress"]
-                    task_id?: string
-                    user_id?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_status_task_id_fkey"
-                        columns: ["task_id"]
-                        isOneToOne: false
-                        referencedRelation: "tasks"
-                        referencedColumns: ["id"]
-                    },
-                    {
-                        foreignKeyName: "public_status_user_id_fkey"
-                        columns: ["user_id"]
-                        isOneToOne: false
-                        referencedRelation: "users"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
-            tasks: {
-                Row: {
-                    created_at: string
-                    due_date: string
-                    group_id: string
-                    id: string
-                    name: string
-                }
-                Insert: {
-                    created_at?: string
-                    due_date: string
-                    group_id: string
-                    id?: string
-                    name: string
-                }
-                Update: {
-                    created_at?: string
-                    due_date?: string
-                    group_id?: string
-                    id?: string
-                    name?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_tasks_group_id_fkey"
-                        columns: ["group_id"]
-                        isOneToOne: false
-                        referencedRelation: "groups"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
-            users: {
-                Row: {
-                    email: string
-                    first_name: string
-                    id: string
-                    last_name: string
-                }
-                Insert: {
-                    email: string
-                    first_name: string
-                    id: string
-                    last_name: string
-                }
-                Update: {
-                    email?: string
-                    first_name?: string
-                    id?: string
-                    last_name?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_users_id_fkey"
-                        columns: ["id"]
-                        isOneToOne: true
-                        referencedRelation: "users"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
-            users_groups: {
-                Row: {
-                    created_at: string
-                    group_id: string
-                    id: string
-                    user_id: string
-                }
-                Insert: {
-                    created_at?: string
-                    group_id: string
-                    id?: string
-                    user_id: string
-                }
-                Update: {
-                    created_at?: string
-                    group_id?: string
-                    id?: string
-                    user_id?: string
-                }
-                Relationships: [
-                    {
-                        foreignKeyName: "public_users_groups_group_id_fkey"
-                        columns: ["group_id"]
-                        isOneToOne: false
-                        referencedRelation: "groups"
-                        referencedColumns: ["id"]
-                    },
-                    {
-                        foreignKeyName: "public_users_groups_user_id_fkey"
-                        columns: ["user_id"]
-                        isOneToOne: false
-                        referencedRelation: "users"
-                        referencedColumns: ["id"]
-                    }
-                ]
-            }
+  public: {
+    Tables: {
+      admins_groups: {
+        Row: {
+          created_at: string
+          group_id: string
+          id: string
+          user_id: string
         }
-        Views: {
-            [_ in never]: never
+        Insert: {
+          created_at?: string
+          group_id: string
+          id?: string
+          user_id: string
         }
-        Functions: {
-            is_user_in_group: {
-                Args: {
-                    _user_id: string
-                    _group_id: string
-                }
-                Returns: boolean
-            }
+        Update: {
+          created_at?: string
+          group_id?: string
+          id?: string
+          user_id?: string
         }
-        Enums: {
-            progress: "Not Started" | "In Progress" | "Completed"
+        Relationships: [
+          {
+            foreignKeyName: "public_admins_groups_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "public_admins_groups_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      groups: {
+        Row: {
+          allow_all: boolean
+          created_at: string
+          created_by: string
+          id: string
+          name: string
+          short_form: string
         }
-        CompositeTypes: {
-            [_ in never]: never
+        Insert: {
+          allow_all: boolean
+          created_at?: string
+          created_by: string
+          id?: string
+          name: string
+          short_form: string
         }
+        Update: {
+          allow_all?: boolean
+          created_at?: string
+          created_by?: string
+          id?: string
+          name?: string
+          short_form?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_groups_created_by_fkey"
+            columns: ["created_by"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      invites: {
+        Row: {
+          created_at: string
+          group_id: string
+          id: string
+        }
+        Insert: {
+          created_at?: string
+          group_id: string
+          id?: string
+        }
+        Update: {
+          created_at?: string
+          group_id?: string
+          id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_invites_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      status: {
+        Row: {
+          created_at: string
+          id: string
+          status: Database["public"]["Enums"]["progress"]
+          task_id: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          status: Database["public"]["Enums"]["progress"]
+          task_id: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          status?: Database["public"]["Enums"]["progress"]
+          task_id?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_status_task_id_fkey"
+            columns: ["task_id"]
+            isOneToOne: false
+            referencedRelation: "tasks"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "public_status_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      tasks: {
+        Row: {
+          created_at: string
+          due_date: string
+          group_id: string
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          due_date: string
+          group_id: string
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string
+          due_date?: string
+          group_id?: string
+          id?: string
+          name?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_tasks_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      users: {
+        Row: {
+          email: string
+          first_name: string
+          id: string
+          last_name: string
+        }
+        Insert: {
+          email: string
+          first_name: string
+          id: string
+          last_name: string
+        }
+        Update: {
+          email?: string
+          first_name?: string
+          id?: string
+          last_name?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_users_id_fkey"
+            columns: ["id"]
+            isOneToOne: true
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      users_groups: {
+        Row: {
+          created_at: string
+          group_id: string
+          id: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          group_id: string
+          id?: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          group_id?: string
+          id?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "public_users_groups_group_id_fkey"
+            columns: ["group_id"]
+            isOneToOne: false
+            referencedRelation: "groups"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "public_users_groups_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
     }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      is_user_in_group: {
+        Args: {
+          _user_id: string
+          _group_id: string
+        }
+        Returns: boolean
+      }
+    }
+    Enums: {
+      progress: "Not Started" | "In Progress" | "Completed"
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
 }
 
 export type Tables<
@@ -248,7 +274,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
     ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-            Row: infer R
+          Row: infer R
         }
         ? R
         : never
@@ -256,7 +282,7 @@ export type Tables<
             Database["public"]["Views"])
         ? (Database["public"]["Tables"] &
             Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-                Row: infer R
+              Row: infer R
             }
             ? R
             : never
@@ -270,13 +296,13 @@ export type TablesInsert<
         : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
     ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-            Insert: infer I
+          Insert: infer I
         }
         ? I
         : never
     : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
         ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-                Insert: infer I
+              Insert: infer I
             }
             ? I
             : never
@@ -290,13 +316,13 @@ export type TablesUpdate<
         : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
     ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-            Update: infer U
+          Update: infer U
         }
         ? U
         : never
     : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
         ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-                Update: infer U
+              Update: infer U
             }
             ? U
             : never
